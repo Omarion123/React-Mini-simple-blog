7286489aefc182eab56ddb9299aa0d540efdc773
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 <anzymen@gmail.com>
2023-10-04 15:33:55 +0200

    use map method and useState hook

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,22 @@
 import { useState } from "react"; // to set react component we use useState
 const Home = () => {
-  const [blogs, setBlogs] = useState([
+  const [blogs, setblogs] = useState([
     {
       title: "My new Website",
-      body: "Lorem ipsum...",
-      author: "marion",
+      body: "Notice the use of %PUBLIC_URL% in the tags above...",
+      author: "mark",
       id: 1,
     },
-    { title: "Welcome party!", body: "Lorem ipsum...", author: "yoshi", id: 2 },
     {
-      title: "Web dev top tops",
-      body: "Lorem ipsum...",
-      author: "mario",
+      title: "My new Wings",
+      body: "Learn how to configure a non-root public URL by running `npm run build...",
+      author: "marion",
+      id: 2,
+    },
+    {
+      title: "My new tooth",
+      body: "If you open it directly in the browser, you will see an empty page...",
+      author: "mari",
       id: 3,
     },
   ]);
@@ -20,7 +25,10 @@ const Home = () => {
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <p>
+            Written by: <span className="author">{blog.author}</span>
+          </p>
+          <p>{blog.body}</p>
         </div>
       ))}
     </div>
